11104809f4fafe1bea2008a3c9f7115d4b563029
daiki0802fujimoto <daiki0802fujimoto@gmail.com>
2023-08-11 19:27:08 +0900

    投稿検索機能追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,11 +10,42 @@ class PostController extends Controller
 {
     public function index(Post $post, Request $request)
     {
+        $posts = Post::query()->paginate(20);
+        $search = $request->input('search');
+        $originSearch = $search['origin'] ?? null;
+        $destinationSearch = $search['destination'] ?? null;
+
+        // クエリビルダ
+        $query = Post::query();
+
+       // もし検索フォームにキーワードが入力されたら
+        // if ($search) {
+
+        //     // 全角スペースを半角に変換
+        //     $spaceConversion = mb_convert_kana($search, 's');
+
+        //     // 単語を半角スペースで区切り、配列にする（例："山田 翔" → ["山田", "翔"]）
+        //     $wordArraySearched = preg_split('/[\s,]+/', $spaceConversion, -1, PREG_SPLIT_NO_EMPTY);
+
+
+        //     // 単語をループで回し、ユーザーネームと部分一致するものがあれば、$queryとして保持される
+        //     foreach($wordArraySearched as $value) {
+        //         $query->where('origin', 'like', '%'.$value.'%');
+        //     }
+            
+        // }
+        $query->where(function ($query) use ($originSearch, $destinationSearch) {
+            if ($originSearch) {
+                $query->orWhere('origin', 'like', '%' . $originSearch . '%');
+            }
+            if ($destinationSearch) {
+                $query->orWhere('destination', 'like', '%' . $destinationSearch . '%');
+            }
+        });
+        $posts = $query->orderBy('updated_at', 'DESC')->paginate(10);
+
         
-        
-        
-        
-        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
+        return view('posts.index')->with(['posts' => $posts, 'originSearch' => $originSearch, 'destinationSearch' => $destinationSearch]);
     }
 
     public function show(Post $post)
