1d835f2f9e84e304171e067d14b41617bbcf90ef
daiki0802fujimoto <daiki0802fujimoto@gmail.com>
2023-10-02 00:18:39 +0900

    シーダーファイル変更

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -11,8 +11,22 @@ use DateTime;
 
 class UserSeeder extends Seeder
 {
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
     public function run()
     {
+        DB::table('users')->insert([
+            'name' => 'test',
+            'email' => 'test@test.com',
+            'password' => Hash::make('hogehoge'),
+            'age' => 25,
+            'sex_id' => 3,
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
         DB::table('users')->insert([
             'name' => 'admin',
             'email' => 'daiki0802fujimoto@gmail.com',
